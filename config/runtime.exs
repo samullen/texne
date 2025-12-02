e9e6d66c0fb60d6c34a1ff571a2b965e7eecb408
Samuel Mullen <samuel@samuelmullen.com>
2025-12-02 14:21:32 -0600

    config: comment out DB_URL

--- config/runtime.exs
+++ config/runtime.exs
@@ -32,11 +32,11 @@ config :texne, :filestore_path, Dotenvy.env!("FILESTORE_PATH")
 
 if config_env() == :prod do
   database_url =
-    System.get_env("DATABASE_URL") ||
-      raise """
-      environment variable DATABASE_URL is missing.
-      For example: ecto://USER:PASS@HOST/DATABASE
-      """
+    System.get_env("DATABASE_URL") #||
+      # raise """
+      # environment variable DATABASE_URL is missing.
+      # For example: ecto://USER:PASS@HOST/DATABASE
+      # """
 
   maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
 
